0f6bbdf71a8a56b22cbec858bb9955c45fa7d3ef
omipawar05-cpu <omipawar05@gmail.com>
2026-04-03 18:39:22 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+# Render will provide this from your Environment Variables
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgresql://postgres:zIXYDXoRaDoXjNPn@db.dwbphotswgudwvnbrjay.supabase.co:5432/postgres')
+db = SQLAlchemy(app)
+
+class Leaderboard(db.Model):
+    id = db.Model.Column(db.Model.Integer, primary_key=True)
+    username = db.Model.Column(db.Model.String(50))
+    score = db.Model.Column(db.Model.Integer)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.json
+    # Basic validation
+    if not data or 'username' not in data or 'score' not in data:
+        return jsonify({"error": "Invalid data"}), 400
+        
+    entry = Leaderboard(username=data['username'], score=data['score'])
+    db.session.add(entry)
+    db.session.commit()
+    return jsonify({"message": "Score saved to Supabase!"}), 201
